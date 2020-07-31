700303810941674d0caa19073c0e3d65f1f7c990
Arthur Andrade <arthur.diegoo@hotmail.com>
2020-07-31 18:31:15 -0300

    :card_file_box: Add Logic and Database

---
+++ assets/database/listaDeVagas.js
@@ -0,0 +1,133 @@
+export default [
+  {
+    id: 1,
+    vaga: "Desenvolvedor Front End Pleno",
+    descrição: `<strong>O que você irá fazer?</strong>
+        </br>
+        Planejar com a equipe a organização do código Front End;
+        Definir e aplicar boas práticas de desenvolvimento;
+        Avaliar junto com os membros da equipe, a adoção de novos componentes e tecnologias;
+        Assegurar que publicação de versões sejam bem sucedidas, atuando em conjunto com as demais equipes que desenvolvem o produto.
+        </br>
+        <strong>As habilidades necessárias</strong>:
+        </br>
+        Ser fera em JavaScript, FERA mesmo;
+        Experiência com Vue.js ou React;
+        Já é ou deseja se tornar apto a liderar uma equipe de desenvolvimento web;
+        Prática em construção de componentes UI, usando protocolo HTTP2, DOM e HTML`,
+    email: {
+      destinatário: "frontend@vagasparacandidatos.com",
+      assunto: "Vaga para Desenvolvedor FrontEnd - Empresa Local",
+    },
+    imagem: "assets/img/jobs/react.png",
+  },
+  {
+    id: 2,
+    vaga: "Desenvolvedor Backend Pleno",
+    descrição: `<strong>O que você irá fazer?</strong>
+    </br>
+    Planejar com a equipe a organização do código Back End;
+    Definir e aplicar boas práticas de desenvolvimento;
+    Avaliar junto com os membros da equipe, a adoção de novos componentes e tecnologias;
+    Assegurar que publicação de versões sejam bem sucedidas, atuando em conjunto com as demais equipes que desenvolvem o produto.
+    </br>
+    <strong>As habilidades necessárias</strong>:
+    </br>
+    Ser fera em JavaScript, FERA mesmo;
+    Experiência com Next.js ou Express.js;
+    Já é ou deseja se tornar apto a liderar uma equipe de desenvolvimento web;
+    Prática em construção de APIs REST e microsserviços`,
+    email: {
+      destinatário: "candidatosempresa2@gmail.com",
+      assunto: "Vaga para Desenvolvedor Backend - Empresa 2",
+    },
+    imagem: "assets/img/jobs/nodejs.png",
+  },
+  {
+    id: 3,
+    vaga: "Devops Pleno",
+    descrição: `<strong>O que você irá fazer?</strong>
+    </br>
+    Planejar com a equipe a organização do código Back End;
+    Definir e aplicar boas práticas de desenvolvimento;
+    Avaliar junto com os membros da equipe, a adoção de novos componentes e tecnologias;
+    Assegurar que publicação de versões sejam bem sucedidas, atuando em conjunto com as demais equipes que desenvolvem o produto.
+    </br>
+    <strong>As habilidades necessárias</strong>:
+    </br>
+    Ser fera em JavaScript, FERA mesmo;
+    Experiência com Next.js ou Express.js;
+    Já é ou deseja se tornar apto a liderar uma equipe de desenvolvimento web;
+    Prática em construção de APIs REST e microsserviços`,
+    email: {
+      destinatário: "candidatos@gmail.com",
+      assunto: "Vaga para Desenvolvedor Devops - Empresa X",
+    },
+    imagem: "assets/img/jobs/docker.png",
+  },
+  {
+    id: 4,
+    vaga: "Tech Lead DevOps",
+    descrição: `<strong>O que você irá fazer?</strong>
+    </br>
+    Planejar com a equipe a organização do código Back End;
+    Definir e aplicar boas práticas de desenvolvimento;
+    Avaliar junto com os membros da equipe, a adoção de novos componentes e tecnologias;
+    Assegurar que publicação de versões sejam bem sucedidas, atuando em conjunto com as demais equipes que desenvolvem o produto.
+    </br>
+    <strong>As habilidades necessárias</strong>:
+    </br>
+    Ser fera em JavaScript, FERA mesmo;
+    Experiência com Next.js ou Express.js;
+    Já é ou deseja se tornar apto a liderar uma equipe de desenvolvimento web;
+    Prática em construção de APIs REST e microsserviços`,
+    email: {
+      destinatário: "candidatos@gmail.com",
+      assunto: "Vaga para Desenvolvedor Devops - Empresa X",
+    },
+    imagem: "assets/img/jobs/docker.png",
+  },
+  {
+    id: 5,
+    vaga: "Desenvolvedor Front End Pleno",
+    descrição: `<strong>O que você irá fazer?</strong>
+    </br>
+    Planejar com a equipe a organização do código FrontEnd;
+    Definir e aplicar boas práticas de desenvolvimento;
+    Avaliar junto com os membros da equipe, a adoção de novos componentes e tecnologias;
+    Assegurar que publicação de versões sejam bem sucedidas, atuando em conjunto com as demais equipes que desenvolvem o produto.
+    </br>
+    <strong>As habilidades necessárias</strong>:
+    </br>
+    Ser fera em JavaScript, FERA mesmo;
+    Experiência com Angular.js;
+    Especialista em UI/UX com conhecimentos fortes de Design`,
+    email: {
+      destinatário: "candidatos@gmail.com",
+      assunto: "Vaga para Desenvolvedor FrontEnd - Empresa X",
+    },
+    imagem: "assets/img/jobs/nodejs.png",
+  },
+  {
+    id: 6,
+    vaga: "Desenvolvedor Front End Pleno",
+    descrição: `<strong>O que você irá fazer?</strong>
+    </br>
+    Planejar com a equipe a organização do código Back End;
+    Definir e aplicar boas práticas de desenvolvimento;
+    Avaliar junto com os membros da equipe, a adoção de novos componentes e tecnologias;
+    Assegurar que publicação de versões sejam bem sucedidas, atuando em conjunto com as demais equipes que desenvolvem o produto.
+    </br>
+    <strong>As habilidades necessárias</strong>:
+    </br>
+    Ser fera em JavaScript, FERA mesmo;
+    Experiência com Next.js ou Express.js;
+    Já é ou deseja se tornar apto a liderar uma equipe de desenvolvimento web;
+    Prática em construção de APIs REST e microsserviços`,
+    email: {
+      destinatário: "candidatos@gmail.com",
+      assunto: "Vaga para Desenvolvedor FrontEnd - Empresa X",
+    },
+    imagem: "assets/img/jobs/react.png",
+  },
+];
